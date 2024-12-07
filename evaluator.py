41d05ea2f2b24f5ce8960ca0b331cd37637b41be
Kyunnilee <heekyung0000@gmail.com>
2024-12-06 17:01:36 -0800

    added question rubric

--- evaluator.py
+++ evaluator.py
@@ -81,8 +81,21 @@ Hallucination: Are the agent's actions completely based on the information it wa
 2. Very few of the agent's actions are backed up by the information it was given. The agent makes a wide array of assumptions and sometimes value its own reasoning over concrete information given to it. 
 1. Almost none of the agent's actions are backed up by the information it was given. The agent mostly ignores the information that it was given and never selects action: lost to try to learn more information. 
 
-Questions Asked:
+Questions Asked: Evaluations are based on the quality of questions asked by the agent when it is lost. Total score for question is calculated as ([proactive] + [clarification])/2:
 
+[Proactive]: Does the agent ask good proactive questions to gather hints for the next step?
+5: The agent consistently asks insightful proactive questions that are relevant and useful.
+4: The agent asks proactive questions that are generally relevant but could be more relevant and useful.
+3: The agent occasionally asks proactive questions, but they often miss the mark or are too generic.
+2: The agent rarely asks proactive questions, and when they do, they are not relevant or useful.
+1: The agent does not ask any proactive questions to gather hints for the next step.
+
+[Clarification]: Does the agent ask clarification questions if the answer received is vague?
+5: The agent always asks for clarifications when responses are vague, ensuring complete understanding for the next step.
+4: The agent usually asks for clarifications on vague responses, but may miss some opportunities.
+3: The agent sometimes asks for clarifications, but often proceeds without full clarity.
+2: The agent rarely seeks clarifications, leading to misunderstandings or incomplete information.
+1: The agent never asks for clarifications, regardless of the clarity of the recevied answers.
 
 '''
 actions_rubric = "Actions Taken: \nHallucination: Are the agent's actions completely based on the information it was given? Does it make any unstated or unreasonable assumptions? 5: All of the agent's actions are backed up by the information it was given. It may make inferences, but all of them were drawn logically from given information. If the agent does not have enough information to choose an action, it always select action: lost. 4. Most of the agent's actions are backed up by the information it was given. There are very few instances of making inferences that are not based on the given information. In rare cases, the agent proceeds with an a different action where action: lost would be more appropriate. 3. Some of the agent's actions are backed up by the information it was given. The agent makes a fair number of assumptions in its reasoning and can be over confident sometimes. However, it still recognizes the information that it was given and uses the relevant data to inform its decision. 2. Very few of the agent's actions are backed up by the information it was given. The agent makes a wide array of assumptions and sometimes value its own reasoning over concrete information given to it.  1. Almost none of the agent's actions are backed up by the information it was given. The agent mostly ignores the information that it was given and never selects action: lost to try to learn more information."
