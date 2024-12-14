a1ee84db4b4c261c075e8e311838f97f1aa4d07a
SimonXie2004 <simonxie2004@163.com>
2024-12-13 18:02:50 -0800

    added mistralai_navi config

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 # Example Usage
-python navigator.py --navi=human_test_navi.json \
+python navigator.py --navi=mistralai_navi.json \
                     --map=touchdown_streetmap.json \
                     --eval=evaluator.json \
                     --task=example_touchdown_task.json \
