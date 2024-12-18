36ab0d99a78df2ad8116dc972d9c3773b8f13553
SimonXie2004 <simonxie2004@163.com>
2024-12-17 19:38:08 -0800

    classified results; now always provide subway info in qa system

--- run_batch.py
+++ run_batch.py
@@ -9,16 +9,18 @@ if __name__ == "__main__":
     cmd += "--vision=openai_vision.json "
 
     task_dir = os.path.join("config", "task", "1215_experiment_1")
+    
     for task_name in os.listdir(task_dir):
-        task_relative_path = os.path.join("1215_experiment_1", task_name)
-        print(f"\n\n======================\nRunning {task_name}\n======================")
-        task_cmd = cmd.replace("<<<task_placeholder>>>", task_relative_path)
-        result = subprocess.run(task_cmd, shell=True, capture_output=True, text=True)
-        
-        print("Output:")
-        print(result.stdout)
-        print("Error:")
-        print(result.stderr)
+        for _ in range(4):
+            task_relative_path = os.path.join("1215_experiment_1", task_name)
+            print(f"\n\n======================\nRunning {task_name}\n======================")
+            task_cmd = cmd.replace("<<<task_placeholder>>>", task_relative_path)
+            result = subprocess.run(task_cmd, shell=True, capture_output=True, text=True)
+            
+            print("Output:")
+            print(result.stdout)
+            print("Error:")
+            print(result.stderr)
 
-        if result.returncode != 0:
-            print(f"Error occurred while running {task_name}.")
+            if result.returncode != 0:
+                print(f"Error occurred while running {task_name}.")
